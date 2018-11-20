579698d18c17d54fbc3e9c40a03f0afae5137ca7
gibeulah <gibeulah@microsoft.com>
2018-11-20 13:14:10 +0530

    Add project files.

---
+++ BinarySearch/ArrayUtility.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    static class ArrayUtility
+    {
+        public static int[] GetArrayFromInputElements() {
+            int[] array = null;
+            Console.WriteLine("Enter the number of elements for the array");
+            try
+            {
+                int noOfElements = int.Parse(Console.ReadLine());
+                array = new int[noOfElements];
+                for (int i = 0; i < noOfElements; i++) {
+                    Console.WriteLine("Enter element "+i);
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+            return array;
+        }
+
+        public static void BinarySearch(int[] array, int element) {
+
+            int index = _BinarySearchUtil(array, 0, array.Length, element);
+            if (index == -1)
+            {
+                Console.WriteLine("Entered element is not found in the array!");
+            }
+            else
+            {
+                Console.WriteLine("The element " + element + " is found at index " + index);
+            }        
+    }
+
+    private static int _BinarySearchUtil(int[] array, int start, int end, int element) {
+
+            while (start <= end) {
+                int mid = (end + start) / 2;
+
+                if (array[mid] == element) {
+                    return mid;
+                }
+
+                if (array[mid] < element) {
+                    return _BinarySearchUtil(array, mid+1, end, element);
+                }
+
+                if (array[mid] > element) {
+                    return _BinarySearchUtil(array, start, mid-1, element);
+                }
+            }
+            return -1;
+        }
+
+        public static bool isSortedArray(int[] array) {
+            for (int i = 0; i< array.Length; i++){
+                for (int j = i + 1; j < array.Length; j++) {
+                    if (array[i] > array[j]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search!");
+            Console.WriteLine("--------------");
+
+            int[] array = ArrayUtility.GetArrayFromInputElements();
+            if (ArrayUtility.isSortedArray(array))
+            {
+                Console.WriteLine("Enter the element to be searched in the sorted array");
+                try
+                {
+                    int elementToSearch = int.Parse(Console.ReadLine());
+                    ArrayUtility.BinarySearch(array, elementToSearch);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Thrown exception is " + exception.Message);
+                }
+            }
+            else {
+
+                Console.WriteLine("Entered array is not sorted!");
+                Console.WriteLine("Binary Search can only work in a sorted array!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
